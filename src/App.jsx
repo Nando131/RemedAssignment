37b0b4a924f82ab2deea15f21902719f4b5bc456
Nando131 <nandoher00@gmail.com>
2025-04-09 19:29:27 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useEffect } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+  <>
+    <div className="Bagianbrand">
+      <div className='Sebelahkiri'>
+        <div className='BagianAtas'>
+            
+            <img src='https://cdn.prod.website-files.com/66d745be9e684f30f3960c6a/66d745be9e684f30f3960ed7_logo-web-brandingfolio-x-webflow-template.svg' alt='BrandFolio'></img>
+
+            <a href='https://brandingfoliotemplate-showcase.webflow.io/home-pages/home-v1' className='Link'> Home </a>
+            <a href='https://brandingfoliotemplate-showcase.webflow.io/company-pages/about'className='Link'> About </a>
+            <a href='https://brandingfoliotemplate-showcase.webflow.io/blog-pages/blog-v1' className='Link'> Blog</a>
+
+            <div className='Link'> Pages </div>
+        </div>
+      </div>
+    </div>
+  </>
+  )
+}
+
+export default App
